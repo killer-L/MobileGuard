8c2100af12fd806bdd439acb8318ce04fe02afb8
lizihao <aa@sss.com>
2017-09-11 14:57:25 +0800

    修改了10000多处，辛苦！！！！

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -9,5 +9,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        System.out.println("hihihih");
+
+
     }
 }
