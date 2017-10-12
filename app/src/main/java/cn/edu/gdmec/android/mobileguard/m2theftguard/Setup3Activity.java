1a0c301a5467de1f60143b6e01b74963b735d53d
killer-L <2304806342@qq.com>
2017-10-13 00:29:17 +0800

    设置向导已完成

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
@@ -0,0 +1,26 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+public class Setup3Activity extends BaseSetUpActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_3);
+        ((RadioButton) findViewById(R.id.rb_third)).setChecked(true);
+    }
+    @Override
+    public void showNext(){
+        startActivityAndFinishSelf(Setup4Activity.class);
+    }
+
+    @Override
+    public void showPre(){
+        startActivityAndFinishSelf(Setup2Activity.class);
+    }
+}
