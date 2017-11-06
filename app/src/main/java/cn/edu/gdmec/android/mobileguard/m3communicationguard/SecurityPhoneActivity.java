8fb9f13b38dc73932792f054d418a55844b69205
Bamboo6 <dream1718@foxmail.com>
2017-11-06 15:19:39 +0800

    fix some bugs

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/SecurityPhoneActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/SecurityPhoneActivity.java
@@ -1,8 +1,8 @@
 package cn.edu.gdmec.android.mobileguard.m3communicationguard;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.FrameLayout;
