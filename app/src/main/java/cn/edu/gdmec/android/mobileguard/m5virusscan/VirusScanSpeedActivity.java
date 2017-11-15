5b96415c84c2ec159e5f7b63fa2720bcabb10225
killer-L <2304806342@qq.com>
2017-11-15 20:44:50 +0800

    完成部分jave代码

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/VirusScanSpeedActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/VirusScanSpeedActivity.java
@@ -1,8 +1,15 @@
 package cn.edu.gdmec.android.mobileguard.m5virusscan;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by killer on 2017/11/13.
  */
 
-public class VirusScanSpeedActivity {
+public class VirusScanSpeedActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
 }
